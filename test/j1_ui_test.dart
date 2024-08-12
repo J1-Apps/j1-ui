7b8e309e68f4283cffb588a8fbc356a8edbc1374
Jaewon Yang <j.yang@draftkings.com>
2024-08-12 17:28:36 -0400

    #2: add github actions

--- test/j1_ui_test.dart
+++ test/j1_ui_test.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
+import "package:flutter_test/flutter_test.dart";
 
-import 'package:j1_ui/j1_ui.dart';
+import "package:j1_ui/j1_ui.dart";
 
 void main() {
-  test('adds one to input values', () {
+  test("adds one to input values", () {
     final calculator = Calculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
