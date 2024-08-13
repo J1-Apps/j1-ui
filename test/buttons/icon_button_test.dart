ebb5f60e322f68461d60e8aff8c3b619aecf41d2
Jaewon Yang <j.yang@draftkings.com>
2024-08-13 10:08:34 -0400

    #6: add icon button test

---
+++ test/buttons/icon_button_test.dart
@@ -0,0 +1,128 @@
+import "package:flutter/material.dart" hide IconButton;
+import "package:flutter_test/flutter_test.dart";
+import "package:j1_ui/j1_ui.dart";
+import "package:mocktail/mocktail.dart";
+
+import "../helpers/mock_callbacks.dart";
+
+void main() {
+  group("Icon Button", () {
+    testWidgets("flat button functions as expected", (tester) async {
+      final onPressed = MockVoidCallback();
+      const smallKey = Key("small");
+      const mediumKey = Key("medium");
+      const largeKey = Key("large");
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Column(
+            children: [
+              IconButton(
+                key: smallKey,
+                icon: JamIcons.h1,
+                type: IconButtonType.flat,
+                size: IconButtonDimens.small,
+                onPressed: onPressed.call,
+              ),
+              IconButton(
+                key: mediumKey,
+                icon: JamIcons.h2,
+                type: IconButtonType.flat,
+                onPressed: onPressed.call,
+                outlineColor: Colors.red,
+                outlineWidth: 1,
+              ),
+              IconButton(
+                key: largeKey,
+                icon: JamIcons.h3,
+                type: IconButtonType.flat,
+                size: IconButtonDimens.large,
+                onPressed: onPressed.call,
+              ),
+            ],
+          ),
+        ),
+      );
+
+      final smallFinder = find.byKey(smallKey);
+      final mediumFinder = find.byKey(mediumKey);
+      final largeFinder = find.byKey(largeKey);
+
+      expect(smallFinder, findsOneWidget);
+      expect(mediumFinder, findsOneWidget);
+      expect(largeFinder, findsOneWidget);
+
+      expect(find.byIcon(JamIcons.h1), findsOneWidget);
+      expect(find.byIcon(JamIcons.h2), findsOneWidget);
+      expect(find.byIcon(JamIcons.h3), findsOneWidget);
+
+      await tester.tap(smallFinder);
+      await tester.tap(mediumFinder);
+      await tester.tap(largeFinder);
+
+      verify(onPressed.call).called(3);
+    });
+
+    testWidgets("filled button functions as expected", (tester) async {
+      final onPressed = MockVoidCallback();
+      const smallKey = Key("small");
+      const mediumKey = Key("medium");
+      const largeKey = Key("large");
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Column(
+            children: [
+              IconButton(
+                key: smallKey,
+                icon: JamIcons.h1,
+                size: IconButtonDimens.small,
+                onPressed: onPressed.call,
+              ),
+              IconButton(
+                key: mediumKey,
+                icon: JamIcons.h2,
+                onPressed: onPressed.call,
+                outlineColor: Colors.red,
+                outlineWidth: 1,
+              ),
+              IconButton(
+                key: largeKey,
+                icon: JamIcons.h3,
+                size: IconButtonDimens.large,
+                onPressed: onPressed.call,
+              ),
+            ],
+          ),
+        ),
+      );
+
+      final smallFinder = find.byKey(smallKey);
+      final mediumFinder = find.byKey(mediumKey);
+      final largeFinder = find.byKey(largeKey);
+
+      expect(smallFinder, findsOneWidget);
+      expect(mediumFinder, findsOneWidget);
+      expect(largeFinder, findsOneWidget);
+
+      expect(find.byIcon(JamIcons.h1), findsOneWidget);
+      expect(find.byIcon(JamIcons.h2), findsOneWidget);
+      expect(find.byIcon(JamIcons.h3), findsOneWidget);
+
+      await tester.tap(smallFinder);
+      await tester.tap(mediumFinder);
+      await tester.tap(largeFinder);
+
+      verify(onPressed.call).called(3);
+    });
+
+    test("dimens is compared correctly", () {
+      const dimens0 = IconButtonDimens.medium;
+      final dimens1 = IconButtonDimens.medium.copyWith();
+      final dimens2 = IconButtonDimens.medium.copyWith(iconSize: Dimens.size_16);
+
+      expect(dimens0 == dimens1, true);
+      expect(dimens0 == dimens2, false);
+    });
+  });
+}
